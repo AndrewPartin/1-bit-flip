4255a5c5ab7c170ecbdd920054e869c2af927930
root <root@BriansLaptop.>
2024-11-09 22:41:15 -0500

    Added keybinds for the buttons

--- buttons.gd
+++ buttons.gd
@@ -7,5 +7,14 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+@warning_ignore("unused_parameter")
 func _process(delta: float) -> void:
 	pass
+
+func _input(event):
+	for i in range(8):
+		if event.is_action_pressed("toggle_"+str(i+1)):
+			var button_mode = get_node(str(i+1))
+			button_mode.set("button_pressed", !button_mode.get("button_pressed"))
+	
+	
